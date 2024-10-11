f7ed05013a58d3bd415b87825530e2f3cf537285
isaurus <isaacmartin.dev@gmail.com>
2024-10-11 17:22:59 +0200

    Final Commit - First Version
    - La aplicación funciona correctamente.
    - El código podría optimizarse utilizando arrays y bucles for (Second Version)

--- app/src/main/java/com/example/valoracionhotel/MainActivity.java
+++ app/src/main/java/com/example/valoracionhotel/MainActivity.java
@@ -46,6 +46,75 @@ public class MainActivity extends AppCompatActivity {
         txtValoracionCalidadPrecio = findViewById(R.id.txtValoracionCalidadPrecio);
         txtValoracionUbicacion = findViewById(R.id.txtValoracionUbicacion);
 
+        // Instancia de TextView para la valoración media
+        txtValoracionMedia = findViewById(R.id.txtValoracionMedia);
 
+        // Listener para cada RatingBar
+        rbPersonal.setOnRatingBarChangeListener((ratingBar, rating, fromUser) -> setValoraciones(1));
+        rbInstalaciones.setOnRatingBarChangeListener((ratingBar, rating, fromUser) -> setValoraciones(2));
+        rbServicios.setOnRatingBarChangeListener((ratingBar, rating, fromUser) -> setValoraciones(3));
+        rbLimpieza.setOnRatingBarChangeListener((ratingBar, rating, fromUser) -> setValoraciones(4));
+        rbConfort.setOnRatingBarChangeListener((ratingBar, rating, fromUser) -> setValoraciones(5));
+        rbCalidadPrecio.setOnRatingBarChangeListener((ratingBar, rating, fromUser) -> setValoraciones(6));
+        rbUbicacion.setOnRatingBarChangeListener((ratingBar, rating, fromUser) -> setValoraciones(7));
+
+        // Vinculación y Listener para el Button Nueva Valoración
+        findViewById(R.id.btnNuevaValoracion).setOnClickListener(v -> reiniciarValoraciones());
+    }
+
+    // Método para asignar el contenido a los TextView de cada RatingBar
+    protected void setValoraciones (int valoracionId){
+
+        switch (valoracionId){
+            case 1:
+                txtValoracionPersonal.setText(String.format("%d", (int)rbPersonal.getRating()));
+                break;
+            case 2:
+                txtValoracionInstalaciones.setText(String.format("%d", (int)rbInstalaciones.getRating()));
+                break;
+            case 3:
+                txtValoracionServicios.setText(String.format("%d", (int)rbServicios.getRating()));
+                break;
+            case 4:
+                txtValoracionLimpieza.setText(String.format("%d", (int)rbLimpieza.getRating()));
+                break;
+            case 5:
+                txtValoracionConfort.setText(String.format("%d", (int)rbConfort.getRating()));
+                break;
+            case 6:
+                txtValoracionCalidadPrecio.setText(String.format("%d", (int)rbCalidadPrecio.getRating()));
+                break;
+            case 7:
+                txtValoracionUbicacion.setText(String.format("%d", (int)rbUbicacion.getRating()));
+                break;
+        }
+        txtValoracionMedia.setText(String.format("%.2f", calcularValoracionMedia()));
+    }
+
+    // Método para calcular la valoración media
+    protected float calcularValoracionMedia(){
+        float valoracionMedia;
+        valoracionMedia = (rbPersonal.getRating()
+                            + rbInstalaciones.getRating()
+                            + rbServicios.getRating()
+                            + rbLimpieza.getRating()
+                            + rbConfort.getRating()
+                            + rbCalidadPrecio.getRating()
+                            + rbUbicacion.getRating())
+                            / 7;
+        return valoracionMedia;
+    }
+
+    // Método para devolver todas las valoraciones a 0
+    protected void reiniciarValoraciones(){
+        rbPersonal.setRating(0);
+        rbInstalaciones.setRating(0);
+        rbServicios.setRating(0);
+        rbLimpieza.setRating(0);
+        rbConfort.setRating(0);
+        rbCalidadPrecio.setRating(0);
+        rbUbicacion.setRating(0);
+
+        txtValoracionMedia.setText("0.0");
     }
 }
